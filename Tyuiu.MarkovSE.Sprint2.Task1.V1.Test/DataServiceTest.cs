6826eca6d486299e0af8aa58b3a39bf4a0d7603b
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 05:07:00 +0500

    Task1

---
+++ Tyuiu.MarkovSE.Sprint2.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MarkovSE.Sprint2.Task1.V1.Lib;
+namespace Tyuiu.MarkovSE.Sprint2.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService ds = new();
+            int a = 119, b = 196, c = 134, d = 327;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            bool[] wait = new bool[6] { true, false, true, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MarkovSE.Sprint2.Task1.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.MarkovSE.Sprint2.Task1.V1.Lib;
+namespace Tyuiu.MarkovSE.Sprint2.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Марков С. Е. | ИСПб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Марков С.Е. | ИСПб-24-1                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и логических операций          *");
+            Console.WriteLine("* (|, &, ||, &&, !, ^), а также арифметических выражений, которая         *");
+            Console.WriteLine("* вернет логическую последовательность (массив): (T, F, T, F, T, F),      *");
+            Console.WriteLine("* при a = 119, b = 196, c = 134, d = 327                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int a = 119, b = 196, c = 134, d = 327;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
