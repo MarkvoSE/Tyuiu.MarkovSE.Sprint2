04c115d72046f73d62b02ad6b0b8991433956b6a
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 05:22:23 +0500

    Task4

---
+++ Tyuiu.MarkovSE.Sprint2.Task4.V20.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.MarkovSE.Sprint2.Task4.V20.Lib;
+namespace Tyuiu.MarkovSE.Sprint2.Task4.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 11;
+            double y = 8;
+            double res = ds.Calculate(x, y);
+            double wait = 3.452;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 199;
+            double res = ds.Calculate(x, y);
+            double wait = 24.995;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
