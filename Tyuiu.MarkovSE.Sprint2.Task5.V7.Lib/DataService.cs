0a1646772536c9a1a405f6ce946ddd3d173dfed0
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 05:29:42 +0500

    Task5

---
+++ Tyuiu.MarkovSE.Sprint2.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,59 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MarkovSE.Sprint2.Task5.V7.Lib
+{
+    public class DataService : ISprint2Task5V7
+    {
+        public string FindMonthName(int startYear, int n)
+        {
+            startYear = 1990;
+            string month;
+            if (n > 12)
+            {
+                n %= 12;
+            }
+            switch (n)
+            {
+                case 0:
+                    month = "январь";
+                    break;
+                case 1:
+                    month = "февраль";
+                    break;
+                case 2:
+                    month = "март";
+                    break;
+                case 3:
+                    month = "аперель";
+                    break;
+                case 4:
+                    month = "май";
+                    break;
+                case 5:
+                    month = "июнь";
+                    break;
+                case 6:
+                    month = "июль";
+                    break;
+                case 7:
+                    month = "август";
+                    break;
+                case 8:
+                    month = "сентябрь";
+                    break;
+                case 9:
+                    month = "октябрь";
+                    break;
+                case 10:
+                    month = "ноябрь";
+                    break;
+                case 11:
+                    month = "декабрь";
+                    break;
+                default:
+                    month = "Неверное число";
+                    break;
+            }
+            return month;
+        }
+    }
+}
---
+++ Tyuiu.MarkovSE.Sprint2.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MarkovSE.Sprint2.Task5.V7.Lib;
+namespace Tyuiu.MarkovSE.Sprint2.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int n = 14;
+            Assert.AreEqual(ds.FindMonthName(1990, n), "Февраль");
+
+        }
+    }
+}
